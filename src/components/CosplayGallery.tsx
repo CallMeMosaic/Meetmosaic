6036dc7c20b2534c5258519d7319952934182f23
CallMeMosaic <Mosaic@mail.rawmosaic.com>
2026-09-14 01:52:41 +0200

    Did more stuff

--- src/components/CosplayGallery.tsx
+++ src/components/CosplayGallery.tsx
@@ -90,7 +90,7 @@ export function CosplayGallery({ isOpen, onClose }: CosplayGalleryProps) {
       images: [
         //{ id: 30, url: '/sorted_assets/cosplay/BS/BS1.jpg', title: 'Sitting and walking' },
         //{ id: 31, url: '/sorted_assets/cosplay/BS/BS2.jpg', title: 'Something moved!' },
-        { id: 32, url: '/sorted_assets/cosplay/BS/BS3.jpg', title: 'Drawing the Gun' },
+        //{ id: 32, url: '/sorted_assets/cosplay/BS/BS3.jpg', title: 'Drawing the Gun' },
         { id: 33, url: '/sorted_assets/cosplay/BS/BS4.jpg', title: 'Approaching' },
         { id: 34, url: '/sorted_assets/cosplay/BS/BS5.jpg', title: 'Spotting' },
         { id: 35, url: '/sorted_assets/cosplay/BS/BS6.jpg', title: 'Close Up' },
